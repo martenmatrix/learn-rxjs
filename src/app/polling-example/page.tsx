67a0a720424f00be790b45a1a81739ee27edcc9f
Marten <derapfelesser@aol.de>
2024-06-30 20:43:20 +0200

    create cat fact polling

--- src/app/polling-example/page.tsx
+++ src/app/polling-example/page.tsx
@@ -1,9 +1,27 @@
 "use client";
 
 import Button from "react-bootstrap/Button";
-import { useState } from "react";
+import React, { useEffect, useState, memo, useCallback } from "react";
+import { ajax } from "rxjs/ajax";
+import {
+  mergeMap,
+  Subject,
+  interval,
+  map,
+  startWith,
+  takeWhile,
+  skipWhile,
+} from "rxjs";
 
-function ConditionalButton({ polling, onClick }) {
+type ConditionalButtonType = {
+  polling: boolean;
+  onClick: () => void;
+};
+
+const ConditionalButton = memo(function ConditionalButton({
+  polling,
+  onClick,
+}: ConditionalButtonType): React.ReactNode {
   return polling ? (
     <Button variant="danger" onClick={onClick}>
       Stop polling
@@ -11,18 +29,34 @@ function ConditionalButton({ polling, onClick }) {
   ) : (
     <Button onClick={onClick}>Start polling</Button>
   );
-}
+});
 
-export default function Polling() {
+export default function Polling(): React.ReactNode {
   const [polling, setPolling] = useState<boolean>(false);
+  const [catFact, setCatFact] = useState<string>("");
 
-  const togglePolling: void = () => {
+  const togglePolling: () => void = useCallback(() => {
     setPolling((prevState) => !prevState);
-  };
+  }, [setPolling]);
+
+  useEffect(() => {
+    const catFacts = interval(10000).pipe(
+      startWith(0),
+      takeWhile(() => polling),
+      mergeMap(() =>
+        ajax
+          .getJSON("https://meowfacts.herokuapp.com/")
+          .pipe(map((data: unknown): string => (data?.data ? data.data : ""))),
+      ),
+    );
+
+    const subscription = catFacts.subscribe(setCatFact);
+    return () => subscription.unsubscribe();
+  }, [polling]);
 
   return (
     <>
-      <p>The app is currently {polling ? "polling" : "not polling"}</p>
+      <h1>{catFact}</h1>
       <ConditionalButton polling={polling} onClick={togglePolling} />
     </>
   );
